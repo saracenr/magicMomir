764ee5ba00aee197da671ddb9fef0e7cf8bde189
saracenr <saracenr@oregonstate.edu>
2019-09-09 19:03:42 -0700

    Completed

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var http = require("http");
+var fs = require("fs");
+var request = require("request");
+var multer = require("multer");
+var upload = multer({dest: 'images/'});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+//  Default landing page
 app.get("/", function(req, res){
 	res.render("momir.ejs");
 });
 
-app.post("/printCard", function(req, res){
-	var magicImage = req.body.magicPicture
-	console.log(magicImage)
+// Post request sending the card url pulled from scryfall API
+app.post("/printCard", upload.single('imageFile'), function(req, res){
+	var imageSource = req.body.imageSRC
+	//  Downloads the file locally to local/images/card.png
+	request.get({url: imageSource, encoding: null}, function (err, response, body) {
+	fs.writeFile("images/card.png", body, null, function(err) {
+    if(err)
+      console.log(err);
+    else
+      console.log("The file was saved!");
+      var exec = require('child_process').exec, child;
+      child = exec('lp -o fit-to-page /home/pi/magic/magic/images/card.png',
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+  }); 
+});
+//	const spawn = require("child_process").spawn;
+//	const pythonProcess = spawn('python3',["/home/pi/magic/magic/pythonPrint.py", magicImage]);
+	console.log(req.body.imageSRC)
 	res.redirect("/");
 });
 
-// app.get("/card/:cmc", function(req, res){
-// 	var manacost = req.params.cmc
-// 	console.log(manacost)
-// 	res.send("The converted mana cost is " + manacost);
-// });
 
 app.get("*", function(req,res){
 	res.send("Invalid Request")
